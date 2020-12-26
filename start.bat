67178f809f13029406b4d183d3cbcd4b58407416
IonZery <46859662+IonZery@users.noreply.github.com>
2020-12-26 12:29:36 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-EthDcrMiner64.exe -epool us1.ethermine.org:4444 -ewal 0x91B381021f83df44945DEd1Fe2026F62A5Ed3489 -epsw x -rxboost 1
+EthDcrMiner64.exe -epool us1.ethermine.org:4444 -ewal 0x6d2f63be4610d06d2fd5d3cad7b3a5073fd99523 -epsw x -rxboost 1
