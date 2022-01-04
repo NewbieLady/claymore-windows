676afad22f97566a3d6f9adce1456dd80a0c2267
NewbieLady <46859662+NewbieLady@users.noreply.github.com>
2022-01-04 11:06:47 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-EthDcrMiner64.exe -epool us1.ethermine.org:4444 -ewal 0x6d2f63be4610d06d2fd5d3cad7b3a5073fd99523 -epsw x -rxboost 1
+EthDcrMiner64.exe -epool us1.ethermine.org:4444 -ewal 0xD6d1238cd4d6438ae5E10046435B73862d6Bb17b -epsw x -rxboost 1
